755e4f1a8fb6d2c7a6c9e9310bb42aab1afbcbbe
Julio Anccasi <julio.anccasi.dev@gmail.com>
2024-05-24 19:53:55 -0500

    Updated Challenge

--- src/com/aluracursos/radioalura/modelo/MisFavoritos.java
+++ src/com/aluracursos/radioalura/modelo/MisFavoritos.java
@@ -1,2 +1,11 @@
-package com.aluracursos.radioalura.modelo;public class MisFavoritos {
+package com.aluracursos.radioalura.modelo;
+
+public class MisFavoritos {
+    public void adicione(Audio audio) {
+        if (audio.getClasificacion() >= 8) {
+            System.out.println(audio.getTitulo() + "Es uno de los favoritos del momento");
+        } else {
+            System.out.println(audio.getTitulo() + "También es uno de los favoritos");
+        }
+    }
 }
